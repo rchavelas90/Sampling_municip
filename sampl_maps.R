3b2b301edbc00e1b34582cc47e13525519a7e41e
rchavelas90 <rchavelas90@gmail.com>
2013-10-18 00:21:15 -0500

    Now shapefile loading into ggplot2 works! yay

--- sampl_maps.R
+++ sampl_maps.R
@@ -2,10 +2,10 @@
 library(ggplot2)
 library(ggmap)
 
-#latit <- mean(df2$latitude) ## latitude
-latit <- 19.43315 # Mexico City Center
-#longit <- mean(df2$longitude) ## longitud
-longit <- -99.13327 # Mexico City Center
+latit <- mean(df2$latitude) ## latitude
+#latit <- 19.43315 # Mexico City Center
+longit <- mean(df2$longitude) ## longitud
+#longit <- -99.13327 # Mexico City Center
 
 mapImageData <-  get_googlemap(center= c(lon = longit, 
                                          lat = latit),
@@ -22,28 +22,36 @@ map <- ggmap(mapImageData,
              xlab = "Longitude",
              legend = "right")
 
+
+## plot all retail stores in Mexico City by "delegación"
+alp <-1/2 # define alppha levelof geom_point  (all data =  0.5)
+siz <-1/2 # define size (all data =  0.5)
+mapa_retail <- map + geom_point(aes(x=longitude,y=latitude,colour=clav_municip),
+                                data=df2,alpha = alp,size=siz)+
+ theme(legend.position="none")# By "delegación"
+mapa_retail + 
+ geom_path(data=municip_shpf_DF_asdf, ## plot the shapefile by Delegación
+                        mapping=aes(long,lat,group=group),
+                        color="#DF3A01")
+
+
+
+
+
 # png("map.png",height=1280,width=1280,pointsize=1,res=300)
 mapa_all <- map + geom_point(aes(x=longitude,y=latitude,colour=clav_municip),data=df,alpha = 1/2,size=0.5)+
  theme(legend.position="none")# By "delegación"
 mapa_all
 # dev.off()
 
-## plot all retail stores in Mexico City by "delegación"
-alp <-1 # define alppha levelof geom_point  (all data =  0.5)
-siz <-2 # define size (all data =  0.5)
-mapa_retail <- map + geom_point(aes(x=longitude,y=latitude,colour=clav_municip),
-                                data=df2,alpha = alp,size=siz)+
- theme(legend.position="none")# By "delegación"
-mapa_retail + geom_path(data=municip_shpf_DF_asdf,
-                        mapping=aes(long,lat,group=group),
-                        color="black")
-
-
 
 ## plot all retail stores in Mexico City by "delegación"
-alp <-1 # define alppha levelof geom_point  (all data =  0.5)
-siz <-2 # define size (all data =  0.5)
+alp <-1/2 # define alppha levelof geom_point  (all data =  0.5)
+siz <-1/2 # define size (all data =  0.5)
 mapa_retail <- ggplot() + geom_point(aes(x=longitude,y=latitude,colour=clav_municip),
                                 data=df2,alpha = alp,size=siz)+
  theme(legend.position="none")# By "delegación"
-mapa_retail
+mapa_retail + 
+ geom_path(data=municip_shpf_DF_asdf, ## plot the shapefile by Delegación
+           mapping=aes(long,lat,group=group),
+           color="#DF3A01")+ coord_equal()
--- sampl_maps_shpf.R
+++ sampl_maps_shpf.R
@@ -1,5 +1,68 @@
+### Loading shape files for "delegaciones", below all the code and links to learn more about shapefiles
 
-## learn to load shapefiles
+
+## Plot IFE Shapefiles WITH RGDAL by entity
+### Tnks unRob for shapfiles
+## Plot  municipalities
+setwd("shapefiles/mar_geog_nac") # set wd in which shapefiles are
+munici_shpf <- readOGR(dsn=".",layer="MUNICIPIO") ## read shapefile "MUNICIPIO"
+
+### change coordinate system of munici_shpf
+### https://stat.ethz.ch/pipermail/r-help/2013-June/355297.html
+## https://stat.ethz.ch/pipermail/r-help/2013-June/355261.html
+## somre references sistems http://www.epsg-registry.org/
+## More on manipulating shapefiles http://live.osgeo.org/en/quickstart/R_quickstart.html
+## http://stackoverflow.com/questions/16462290/obtaining-latitude-and-longitude-with-from-spatial-objects-in-r
+summary(munici_shpf)
+is.projected(munici_shpf)
+proj4string(munici_shpf) ## get the coordinate system assigned to the shapefile
+head(municip_shpf_DF_asdf)
+munici_shpf <- spTransform(munici_shpf,CRS("+proj=longlat +datum=WGS84")) ## this changes the projectio
+
+### get only DF shapefile
+ogrInfo(".",layer="MUNICIPIO") # wet info of shapefile
+class(munici_shpf)
+plot(munici_shpf)
+head(munici_shpf@data)
+# row.names(munici_shpf)
+str(munici_shpf@polygons[1],max.level=4)
+filter_mex_city <- which(munici_shpf@data$ENTIDAD==9)
+municip_shpf_DF <- munici_shpf[filter_mex_city,]
+str(municip_shpf_DF@polygons,max.level=3)
+row.names(municip_shpf_DF) <- as.character(municip_shpf_DF$NOMBRE)
+## municip_df <- munici_shpf@data[munici_shpf@data$ENTIDAD==9,]
+municip_shpf_DF@data
+plot(municip_shpf_DF,axes=T,border="gray")
+
+##use that shapefile to plot in GGPLOT2
+require("rgdal") # requires sp, will use proj.4 if installed
+require("maptools")
+require("ggplot2")
+require("plyr")
+municip_shpf_DF_asdf <- fortify(municip_shpf_DF) ## Transform into a Data frame
+head(municip_shpf_DF_asdf,10)
+
+## plot all "municipios"
+ggplot(municip_shpf_DF_asdf) + 
+ aes(long,lat,group=group,fill=id) + 
+ geom_polygon() +
+ geom_path(color="white") +
+ coord_equal() 
+
+## plot all "municipios" // Different structure
+p <- ggplot()+
+ geom_polygon(data=municip_shpf_DF_asdf,
+              mapping=aes(long,lat,group=group,fill=id))+
+ coord_equal() 
+p+ geom_path(data=municip_shpf_DF_asdf,
+             mapping=aes(long,lat,group=group),
+             color="grey")
+
+
+
+
+
+################ learn to load shapefiles ################
 ## useful resources 
 ## https://github.com/hadley/ggplot2/wiki/plotting-polygon-shapefiles
 ## http://www.nceas.ucsb.edu/scicomp/usecases/ReadWriteESRIShapeFiles
@@ -61,55 +124,10 @@ row.names(entidad_shpf_DF) <- as.character(entidad_shpf_DF$NOMBRE)
 plot(entidad_shpf_DF,axes=T,border="gray") ## plot the shapefile
 
 
-## Plot IFE Shapefiles WITH RGDAL by entity
-## Plot  municipalities
-setwd("shapefiles/mar_geog_nac") # set wd in which shapefiles are
-munici_shpf <- readOGR(dsn=".",layer="MUNICIPIO") ## read shapefile "MUNICIPIO"
-ogrInfo(".",layer="MUNICIPIO") # wet info of shapefile
-class(munici_shpf)
-plot(munici_shpf)
-head(munici_shpf@data)
-row.names(munici_shpf)
-str(munici_shpf@polygons[1],max.level=3)
-filter_mex_city <- which(munici_shpf@data$ENTIDAD==9)
-municip_shpf_DF <- munici_shpf[filter_mex_city,]
-str(municip_shpf_DF@polygons,max.level=3)
-row.names(municip_shpf_DF) <- as.character(municip_shpf_DF$NOMBRE)
-## municip_df <- munici_shpf@data[munici_shpf@data$ENTIDAD==9,]
-municip_shpf_DF@data
-plot(municip_shpf_DF,axes=T,border="gray")
 
 
-##use that shapefile to plot in GGPLOT2
-require("rgdal") # requires sp, will use proj.4 if installed
-require("maptools")
-require("ggplot2")
-require("plyr")
-municip_shpf_DF_asdf <- fortify(municip_shpf_DF)
-head(municip_shpf_DF_asdf,10)
 
-ggplot(municip_shpf_DF_asdf) + 
- aes(long,lat,group=group,fill=id) + 
- geom_polygon() +
- geom_path(color="white") +
- coord_equal() 
 
-p <- ggplot()+
- geom_polygon(data=municip_shpf_DF_asdf,
-              mapping=aes(long,lat,group=group,fill=id))+
- coord_equal() 
-p+ geom_path(data=municip_shpf_DF_asdf,
-             mapping=aes(long,lat,group=group),
-             color="black")
 
-###try to change coordinate system of munici_shpf
-### or directly to municip_shpf_DF_asdf
-### https://stat.ethz.ch/pipermail/r-help/2013-June/355297.html
-summary(munici_shpf)
-is.projected(munici_shpf)
-proj4string(munici_shpf)
-head(municip_shpf_DF_asdf)
 
-?spTransform
-?proj4string
 
